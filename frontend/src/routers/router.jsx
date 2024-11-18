045b18664a2710cbe397170d4bb96e2d2177a282
“Ling <lzhuo2@ualberta.ca>
2024-11-17 22:38:28 -0700

    Register Page

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -7,6 +7,7 @@ import Search from "../pages/search/Search";
 import ShopPage from "../pages/shop/ShopPage";
 import SingleProduct from "../pages/shop/productDetails/SingleProduct";
 import Login from '../components/Login'
+import Register from '../components/Register'
 
 const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
     path: "/login",
     element: <Login />
   },
+  {
+		path: '/register',
+		element: <Register />,
+	},
 ]);
 
 export default router;
